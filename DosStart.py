d4ae9e8d4a2eb9d95e4521f85d3bfcc13b96ea05
! Lil x Sey <92247496+LilxxxSey@users.noreply.github.com>
2022-01-06 15:41:04 -0700

    Update DosStart.py

--- DosStart.py
+++ DosStart.py
@@ -4,7 +4,7 @@ import sys
 
 num = '0123456789'
 file = open('phone.txt', 'a')
-code = input('[+] Port: ')
+code = input('[+] Introduzca la cantidad: ')
     
 for a in num:
     for b in num:
@@ -14,7 +14,7 @@ for a in num:
                     for f in num:
                         for g in num:
                             for h in num:
-                                number = 'Packete enviados:  ' +code+a+b+c+d+e+f+g+h
+                                number = 'Packetes enviados:  ' +code+a+b+c+d+e+f+g+h
                                 file.write(number+'\n')
                                 print(number)
 file.close()                               
